2ee3435c0e118390d55d039a298c3e19e082a441
Ranjit2143 <74855990+Ranjit-Singh786@users.noreply.github.com>
2022-01-21 16:48:11 +0530

    Add files via upload

---
+++ function.php
@@ -0,0 +1,15 @@
+<?php
+
+function createDiv($color,$height,$backgroundcolor){
+    //$color = 'color';
+    echo "<div style='color:$color; height:$height;background-color:$backgroundcolor'>xyz</div>";
+}
+  createDiv('black','100','red');
+
+
+
+
+
+
+?>
+<!-- <div style="color:white;height:50px;background-color:red;">xyz</div> -->
